91cef30313a5b0f22a52fefe757fc19d9d360cc1
RAlanWright <wright.alan88@gmail.com>
2021-04-07 09:58:16 -0500

    Added initial laravel project and some posts with routes (WIP)

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post"> My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post"> My Third Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
+        </p>
+    </article>
+
+
+
+</body>
+</html>
