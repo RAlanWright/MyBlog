4da209169f2b4dc6a99aa76d0411ef8472377340
RAlanWright <wright.alan88@gmail.com>
2021-04-07 11:02:17 -0500

    Set route to target slugs, return 404, and return home for routing

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
     <article>
-        <h1><a href="/post"> My First Post</a></h1>
+        <h1><a href="/posts/my-first-post"> My First Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
@@ -19,7 +19,7 @@
     </article>
 
     <article>
-        <h1><a href="/post"> My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post"> My Second Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
@@ -27,7 +27,7 @@
     </article>
 
     <article>
-        <h1><a href="/post"> My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post"> My Third Post</a></h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at autem consequatur dignissimos distinctio inventore, iure maxime minima mollitia, nesciunt officiis pariatur placeat quas, ratione repellat sit tenetur ullam? Veniam.
         </p>
