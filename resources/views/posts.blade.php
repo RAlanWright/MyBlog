c75e6c68581b793342386a175d8a3b80ee06d9f4
RAlanWright <wright.alan88@gmail.com>
2021-04-25 12:42:29 -0500

    Implemented basic blade templating

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,17 +10,17 @@
 
 </head>
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
+    @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'mt-6' : '' }}">
             <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
             <div>
-                <?= $post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
         </article>
-    <?php endforeach; ?>
+    @endforeach
 </body>
 </html>
